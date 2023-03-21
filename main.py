427072f072325dbdb58bcbf7451077d70288be95
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-03-21 13:54:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+def add(x, y):
+  """Adding numbers"""
+  return x + y
+  
+def subtract(x, y):
+  """Subtracts numbers"""
+  return x - y
+  
+def multiply(x, y):
+  """Multiplies numbers"""
+  return x * y
+  
+def divide(x, y):
+  """Divides numbers"""
+  return x / y
+  
+operations = {
+  "+":add,
+  "-":subtract,
+  "*":multiply,
+  "/":divide
+}
+
+x = int(input("What's the first number?\n"))
+for symbol in operations:
+  print(symbol)
+operation_symbol = input("Pick the operation from the line above?\n")
+y = int(input("What's the second number?\n"))
+calculation_function = operations[operation_symbol]
+first_answer = int(calculation_function(x, y))
+print(f"{x} {operation_symbol} {y} = {first_answer}")
+
+continue_process = True
+
+decision = input(f"Would you like to continue the process with the number - {first_answer}? type y or n.\n")
+continue_process = True
+while continue_process:
+    if decision == 'n':
+      continue_process = False
+      print('Goodbye')
+    else:
+      operation_symbol = input("Pick another operation: ")
+      z = int(input("What's the next number?\n"))
+      calculation_function = operations[operation_symbol]
+      second_answer = int(calculation_function(first_answer, z))
+      print(f"{first_answer} {operation_symbol} {z} = {second_answer}")
+      first_answer = second_answer
+      decision = input(f"Would you like to continue the process with the number - {second_answer}? type y or n.\n")
