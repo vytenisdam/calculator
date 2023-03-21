c4895577e2aa3e8c3b1e0659ca265eb71128061a
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-03-21 14:07:56 +0200

    Added recursion

--- main.py
+++ main.py
@@ -20,29 +20,31 @@ operations = {
   "*":multiply,
   "/":divide
 }
+def calculator():
+    x = int(input("What's the first number?\n"))
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Pick the operation from the line above?\n")
+    y = int(input("What's the second number?\n"))
+    calculation_function = operations[operation_symbol]
+    first_answer = int(calculation_function(x, y))
+    print(f"{x} {operation_symbol} {y} = {first_answer}")
 
-x = int(input("What's the first number?\n"))
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Pick the operation from the line above?\n")
-y = int(input("What's the second number?\n"))
-calculation_function = operations[operation_symbol]
-first_answer = int(calculation_function(x, y))
-print(f"{x} {operation_symbol} {y} = {first_answer}")
+    continue_process = True
 
-continue_process = True
-
-decision = input(f"Would you like to continue the process with the number - {first_answer}? type y or n.\n")
-continue_process = True
-while continue_process:
-    if decision == 'n':
-      continue_process = False
-      print('Goodbye')
-    else:
-      operation_symbol = input("Pick another operation: ")
-      z = int(input("What's the next number?\n"))
-      calculation_function = operations[operation_symbol]
-      second_answer = int(calculation_function(first_answer, z))
-      print(f"{first_answer} {operation_symbol} {z} = {second_answer}")
-      first_answer = second_answer
-      decision = input(f"Would you like to continue the process with the number - {second_answer}? type y or n.\n")
+    decision = input(f"Would you like to continue the process with the number - {first_answer}? type y or n.\n")
+    continue_process = True
+    while continue_process:
+        if decision == 'n':
+            continue_process = False
+            calculator()
+        else:
+            operation_symbol = input("Pick another operation: ")
+            z = int(input("What's the next number?\n"))
+            calculation_function = operations[operation_symbol]
+            second_answer = int(calculation_function(first_answer, z))
+            print(f"{first_answer} {operation_symbol} {z} = {second_answer}")
+            first_answer = second_answer
+            decision = input(f"Would you like to continue the process with the number - {second_answer}? type y or n.\n")
+        
+calculator()
