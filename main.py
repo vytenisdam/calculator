8711237f9c10db38128248ad2f265fb732b7976b
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-03-21 14:22:42 +0200

    Added art, console clear. Debuged float data type.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from art import logo
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+    
 def add(x, y):
   """Adding numbers"""
   return x + y
@@ -20,14 +26,16 @@ operations = {
   "*":multiply,
   "/":divide
 }
+
 def calculator():
-    x = int(input("What's the first number?\n"))
+    print(logo)
+    x = float(input("What's the first number?\n"))
     for symbol in operations:
         print(symbol)
     operation_symbol = input("Pick the operation from the line above?\n")
-    y = int(input("What's the second number?\n"))
+    y = float(input("What's the second number?\n"))
     calculation_function = operations[operation_symbol]
-    first_answer = int(calculation_function(x, y))
+    first_answer = float(calculation_function(x, y))
     print(f"{x} {operation_symbol} {y} = {first_answer}")
 
     continue_process = True
@@ -37,12 +45,13 @@ def calculator():
     while continue_process:
         if decision == 'n':
             continue_process = False
+            cls()
             calculator()
         else:
             operation_symbol = input("Pick another operation: ")
-            z = int(input("What's the next number?\n"))
+            z = float(input("What's the next number?\n"))
             calculation_function = operations[operation_symbol]
-            second_answer = int(calculation_function(first_answer, z))
+            second_answer = float(calculation_function(first_answer, z))
             print(f"{first_answer} {operation_symbol} {z} = {second_answer}")
             first_answer = second_answer
             decision = input(f"Would you like to continue the process with the number - {second_answer}? type y or n.\n")
